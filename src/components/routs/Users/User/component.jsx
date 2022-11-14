b62ecd8122137887e72f8898f6afdf3f6ce89775
sydorova-nataliya <nsidorova200363@gmail.com>
2022-11-14 23:04:49 +0300

    add detailed info about user

--- src/components/routs/Users/User/component.jsx
+++ src/components/routs/Users/User/component.jsx
@@ -7,21 +7,34 @@ import './styles.scss'
 const Users=()=>  {
 
   let {userId} = useParams();
-  console.log(userId);
-
-  let data = useGetData(`https://jsonplaceholder.typicode.com/users/${userId}`);
-  console.log(data);
-
-  if(!userId){
+  if(userId===undefined){
+    return ;
+  }
+    let {data, _, loading} = useGetData(`https://jsonplaceholder.typicode.com/users/${userId}`, [userId]);
+    console.log(data);
+    const {id, name, username,email, address:{street,suite, city, zipcode,geo:{lat, lng}},phone,website,company:{name:companyName, catchPhrase,bs}} = data;
+    if(!userId){
+      return(
+          <p>You can select "More information" in the user to get business information about this user</p>
+      )
+    }
+  
     return(
-        <p>You can select "More information" in the user to get business information about this user</p>
+      <div className="info">
+        <h4 className="info__title">Detailed infrormation about {name}</h4>
+        <address className="info__adress">
+         <span className="bold">Address: </span>{city}, {street}, {suite} <br/> 
+         <span className="bold">Zipcode: </span>{zipcode} <br/> 
+         <span className="bold">Geo: </span>{lat} {lng}
+        </address>
+        <a href="#" className="info__tel"><span className="bold">Phone: </span>{phone}</a> <br/>
+        <a href="#" className="info__website">{website}</a>
+        <p className="info__company"><span className="bold">Company: </span>{companyName}</p>
+      </div>
     )
   }
 
-  return(
-    <div>{data.username}</div>
-  )
-}
+
 
 export default Users;
 
--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -1,14 +1,23 @@
 import { useEffect, useState } from "react"
 
-const useGetData=(url)=>{
+const useGetData=(url, deps = [])=>{
     const [data, setData] = useState([]);
-
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    
     useEffect(()=>{
-        fetch(url)
-            .then(res=>res.json())
-            .then(data=>setData(data))
-        },[])
-    return data
-}
+        setLoading(true);
+    fetch(url)
+        .then(res=>res.json())
+        .then(data=>setData(data))
+        .catch(error=>setError(error))
+        .finally(()=> setLoading(false))
+    },deps)
 
+    return{
+        data,
+        error,
+        loading
+    }
+}
 export default useGetData;
