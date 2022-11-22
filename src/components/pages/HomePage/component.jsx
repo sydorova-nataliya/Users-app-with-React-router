97893452abe545dcecbaa4a76fe0083d4ddd0f09
sydorova-nataliya <nsidorova200363@gmail.com>
2022-11-23 00:55:13 +0300

    fixed routes

--- src/components/pages/HomePage/component.jsx
+++ src/components/pages/HomePage/component.jsx
@@ -6,9 +6,8 @@ import Users from "../../routs/Users";
 import User from "../../routs/Users/User";
 import UserIndex from "../../routs/Users/UserIndex";
 import Posts from "../../routs/Posts";
-import PostIndex from "../../routs/Posts/PostIndex";
-
 import './styles.scss'
+
 
 const HomePage=()=>  (
       <>
@@ -19,10 +18,7 @@ const HomePage=()=>  (
               <Route index element={<UserIndex />}/>
               <Route path =":userId" element={<User />} />
             </Route> 
-            <Route path="/posts" element={<Posts/>}> 
-              <Route index element={<PostIndex/>}/>
-            </Route>
-            
+            <Route path="/posts" element={<Posts/>}/> 
             </Route>
 
           <Route path="*" element={<Error />} />
--- src/components/routs/Posts/PostIndex/component.jsx
+++ src/components/routs/Posts/PostIndex/component.jsx
@@ -1,7 +1,11 @@
 import './styles.scss'
 
 const PostIndex=()=> (
-        <p>You can enter Title, Body and User Id to create your own post!</p>
+        <div>
+            <p>Now there is no posts!</p>
+            <p>You can enter Title, Body and User Id to create your own post!</p>
+        </div>
+        
     )
 
 export default PostIndex;
